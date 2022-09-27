870968d6f10cb99a7f39ebeeacfe210c697acc45
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2022-09-28 02:53:51 +0900

    実行前にdownするようにした

--- run_data_migration.sh
+++ run_data_migration.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+#
+# Run data migration
+#
+# usage:
+#   DOCKER_HOST="unix:///var/run/docker.sock" COMPOSE_PROJECT_NAME="ytdlor" ./run_data_migration.sh
+#
+
 set -e
 
 echo "DOCKER_HOST: ${DOCKER_HOST:?}" >&2
 echo "COMPOSE_PROJECT_NAME: ${COMPOSE_PROJECT_NAME:?}" >&2
 
+docker compose -f docker-compose.yml -f docker-compose-data-migration.yml down
 docker compose -f docker-compose.yml -f docker-compose-data-migration.yml build
 docker compose -f docker-compose.yml -f docker-compose-data-migration.yml run --rm data_migration
 docker compose -f docker-compose.yml -f docker-compose-data-migration.yml down
