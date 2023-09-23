40ada7ce03b6854ab7394c4bfc29601d0b0f01d4
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2023-09-24 03:27:51 +0900

    modelのテストを実装した

--- test/models/archive_test.rb
+++ test/models/archive_test.rb
@@ -12,7 +12,36 @@
 require "test_helper"
 
 class ArchiveTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @archive = Archive.new(original_url: "https://vimeo.com/1084537")
+  end
+
+  test "should be valid" do
+    assert @archive.valid?
+  end
+
+  test "original_url should be present" do
+    @archive.original_url = ""
+    assert_not @archive.valid?
+  end
+
+  test "should get title" do
+    @archive.save!
+    @archive.update_title
+    assert_equal "Big Buck Bunny", @archive.title
+  end
+
+  test "should get thumbnail" do
+    # broadcast_replace_to がレンダリングするviewからidが参照されるので, DBに保存しておく
+    @archive.save!
+    @archive.update_thumbnail
+    assert @archive.thumbnail.attached?
+  end
+
+  test "should get video" do
+    # broadcast_replace_to がレンダリングするviewからidが参照されるので, DBに保存しておく
+    @archive.save!
+    @archive.update_video
+    assert @archive.video.attached?
+  end
 end
